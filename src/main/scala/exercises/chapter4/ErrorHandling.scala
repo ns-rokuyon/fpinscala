48d022e958a66e726db1490cd1662ce5f1a01606
ns64 <ness3psi@gmail.com>
2015-05-25 22:40:26 +0900

    add Either base

--- src/main/scala/exercises/chapter4/ErrorHandling.scala
+++ src/main/scala/exercises/chapter4/ErrorHandling.scala
@@ -71,3 +71,27 @@ object Option {
 
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B]
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B]
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C]
+}
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def Try[A](a: => A): Either[Exception, A] = {
+    try Right(a)
+    catch {
+      case e: Exception => Left(e)
+    }
+  }
+
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+}
