13db6b8692a247beb77eee8e75c2db4fa633beda
ns64 <ness3psi@gmail.com>
2015-06-03 22:30:58 +0900

    add Stream class

---
+++ src/main/scala/exercises/chapter5/Laziness.scala
@@ -0,0 +1,19 @@
+package exercises.chapter5
+
+/**
+ * Created by ns64 on 2015/06/03.
+ */
+trait Stream[+A]
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
