67ba7f8590f01fd6d9aec305a4218a9b020a8ea2
ns64 <ness3psi@gmail.com>
2015-07-03 23:48:35 +0900

    add chapter6

---
+++ src/main/scala/exercises/chapter6/State.scala
@@ -0,0 +1,17 @@
+package exercises.chapter6
+
+/**
+ * Created by ns64 on 2015/07/03.
+ */
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
